31dc6a18dabf8c6e9cc80b322cf74c7ebb9f4398
meansori <himeansori@gmail.com>
2025-09-29 09:50:22 +0700

    setup prod

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 4001;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 // Middleware
 app.use(cors());
+// CORS configuration untuk production
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === "production" ? ["http://31.97.109.215", "http://localhost:3000"] : "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Routes
